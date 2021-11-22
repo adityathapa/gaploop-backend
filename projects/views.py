991394f7679adec5c6b9887ee9812fa800303b2d
Aditya <aditya.th9098@gmail.com>
2021-11-22 06:42:50 +0530

    first commit

---
+++ projects/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def projects(request):
+    return render(request, 'projects/projects.html')
+
+def project(request, pk):
+    return render(request, 'projects/single-project.html')
